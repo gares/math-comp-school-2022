2ca322295c2876ddf07247d41a7dae1d3b30dbd2
Kazuhiko Sakaguchi <pi8027@gmail.com>
2022-12-08 01:16:08 +0100

    WIP Lesson 8

--- lesson8.v
+++ lesson8.v
@@ -2,7 +2,7 @@ From Coq Require Import ZifyClasses ZArith_base.
 From elpi Require Import elpi.
 From HB Require Import structures.
 From mathcomp Require Import all_ssreflect ssralg ssrnum ssrint rat intdiv.
-From mathcomp Require Import zify ring lra.
+From mathcomp Require Import ssrZ zify ring lra.
 
 Set Implicit Arguments.
 Unset Strict Implicit.
@@ -58,8 +58,7 @@ Local Delimit Scope Z_scope with coqZ.
 This tactic provides a certifying decision procedure for quantifier-free linear
 integer arithmetic.
 
-<div>
-*)
+<div> *)
 Goal forall m n : nat, n <= m -> n.*2 <= m + n.
 Proof.
 lia.
@@ -72,12 +71,16 @@ Qed.
 
 Goal forall m n : int, (n <= m -> n * 2 <= m + n)%R.
 Proof. lia. Qed.
+
+(* A complicated example taken from the Coq reference manual. *)
+Goal forall m n : int,
+  (27 <= 11 * m + 13 * n <= 45)%R -> (-10 <= 7 * m - 9 * n <= 4)%R -> False.
+Proof. lia. Qed.
 (** #</div>#
 
 The [zify] and [lia] tactics support some [bool] operators.
 
-#<div>#
-*)
+#<div># *)
 Goal forall m n : nat, (n <= m) = ~~ (m.*2 < n.*2).
 Proof. lia. Qed.
 (** #</div></div># *)
@@ -89,8 +92,7 @@ Proof. lia. Qed.
 The [lia] tactic performs normalization with respect to (semi)ring axioms, and
 can actually solve some non-linear problems.
 
-#<div>#
- *)
+#<div># *)
 Goal forall m n : nat, m * n = n * m.
 Proof. lia. Qed.
 
@@ -101,8 +103,7 @@ Proof. lia. Qed.
 The Mczify library also pre-processes Euclidean division and the divisibility
 relation.
 
-#<div>#
-*)
+#<div># *)
 Goal forall m : nat, m %/ 2 <= m.
 Proof.
 lia.
@@ -128,8 +129,7 @@ The [nia] tactic is an extension of the [lia] tactic that allows for some
 non-linear reasoning by propagating positivity and negativity conditions through
 multiplication and exponentiation.
 
-#<div>#
-*)
+#<div># *)
 Goal forall m n : int, (0 <= m -> 0 <= n -> 0 <= m * n)%R.
 Proof. nia. Qed.
 
@@ -153,8 +153,7 @@ Abort.
 
 The [zify] and [lia] tactics do not recognize user-defined operators out of the box:
 
-#<div>#
-*)
+#<div># *)
 Definition triple (n : nat) : nat := n * 3.
 
 Goal forall n, triple (triple n) = n * 9.
@@ -167,8 +166,7 @@ Abort.
 Declare an instance of type [UnOp triple], and register it through the
 [Add Zify UnOp] command:
 
-#<div>#
-*)
+#<div># *)
 Fact Op_triple_subproof (n : nat) : Z.of_nat (triple n) = (3 * Z.of_nat n)%coqZ.
 Proof. rewrite /triple; lia. Qed.
 
@@ -180,8 +178,7 @@ Add Zify UnOp Op_triple.
 
 Then, the [zify] tactic starts recognizing the new operator [triple]:
 
-#<div>#
-*)
+#<div># *)
 Goal forall n, triple (triple n) = n * 9.
 Proof. lia. Qed.
 (** #</div></div># *)
@@ -193,16 +190,14 @@ Proof. lia. Qed.
 This tactic provides a certified decision procedure for polynomial equations
 over commutative rings.
 
-#<div>#
-*)
+#<div># *)
 Goal forall a b : int, ((a + b) ^+ 2 = a ^+ 2 + b ^+ 2 + 2 * a * b :> int)%R.
 Proof. move=> a b; ring. Qed.
 (** #</div>#
 
 It works for any abstract or concrete commutative rings:
 
-#<div>#
-*)
+#<div># *)
 Goal forall a b : rat, ((a + b) ^+ 2 = a ^+ 2 + b ^+ 2 + 2 * a * b :> rat)%R.
 Proof. move=> a b; ring. Qed.
 
@@ -221,18 +216,20 @@ Proof. move=> R a b; ring. Qed.
 
 The [ring] tactic can prove polynomial equations modulo monomial equalities:
 
-#<div>#
-*)
+#<div># *)
 Goal forall a b : int, (a * b = 15 -> (a + b) ^+ 2 = a ^+ 2 + b ^+ 2 + 30)%R.
-Proof. move=> a b H; ring: H. Qed.
+Proof.
+move=> a b H.
+ring: H.
+Qed.
 (** #</div>#
 
-The [ring] tactic can also automatically push down homomorphism applications:
+The Algebra Tactics library implements preprocessors to push down homomorphism
+applications:
 
-#<div>#
-*)
+#<div># *)
 Goal forall (R : ringType) (S : comRingType) (f : {rmorphism R -> S}) (a b : R),
-    (f ((a + b) ^+ 2) = f a ^+ 2 + f b ^+ 2 + 2%:R * f a * f b)%R.
+    (f ((a + b) ^+ 2) = f a ^+ 2 + f b ^+ 2 + 2 * f a * f b)%R.
 Proof.
 move=> R S f a b.
 rewrite rmorphX rmorphD. (* This line can be removed. *)
@@ -244,39 +241,169 @@ Qed.
 
 ** [field]: rational equation solver
 
-#<div>#
+This tactic provides a certified decision procedure for rational equations over
+fields.
+
+#<div># *)
+Goal forall (F : fieldType) (x : F),
+    (x != 0 -> (1 - 1 / x) * x - x + 1 = 0)%R.
+Proof.
+move=> F x x_neq0.
+field.
+(*
+The field tactic leaves a proof obligation saying that the denominators in the
+input are non-zero.
 *)
+exact: x_neq0.
+Qed.
 
-(* TODO *)
+Goal forall (F : fieldType) (n : nat),
+    (n%:R != 0 :> F -> (2 * n)%:R / n%:R = 2 :> F)%R.
+Proof.
+move=> F n n_neq0.
+field.
+exact: n_neq0.
+Qed.
+(** #</div>#
 
-(** #</div></div># *)
+For a partially ordered field ([numFieldType]), non-nullity conditions of the
+form [n%:R != 0] and [n%:~R != 0] where [n] is a non-zero constant are trivial,
+and thus are not generated.
+
+#<div># *)
+Goal forall (F : numFieldType) (x : F), ((x / 2) * 2)%R = x.
+Proof.
+move=> F x.
+field.
+by [].
+Qed.
+(** #</div>#
+
+As in the [ring] tactic, the [field] tactic supports homomorphisms and reasoning
+modulo monomial equalities.
+
+#</div># *)
 (** -------------------------------------------- *)
 (** #<div class='slide'>#
 
 ** [lra] and [nra]: linear and non-linear real arithmetic
 
-#<div>#
-*)
+NB: The support for the [lra] and [nra] tactics in Algebra Tactics is
+experimental for the moment.
 
-(* TODO *)
+#<div># *)
+Goal forall (x y : int), (y <= x -> 0 <= x -> y <= x * 2)%R.
+Proof. move=> R x y; lra. Qed.
+(** #</div>#
 
+For [realFieldType]s, division and multiplicative inverse are avairable.
+
+#<div># *)
+Goal forall (x y : rat), (y <= x -> 0 <= x -> y / 2 <= x)%R.
+Proof. move=> R x y; lra. Qed.
+(** #</div>#
+
+These tactics work for any abstract or concrete [realDomainType] or
+[realFieldType].
+
+#<div># *)
+Goal forall (R : realDomainType) (x y : R),
+  (x + 2 * y <= 3 -> 2 * x + y <= 3 -> x + y <= 2)%R.
+Proof. move=> R x y; lra. Qed.
 (** #</div></div># *)
 (** -------------------------------------------- *)
 (** #<div class='slide'>#
 
 ** How do they work? -- proof by large-scale reflection
 
-#<div>#
-*)
+We first prove the following reflection lemma (but do not try to understand the
+formal statement and proof).
 
-(* TODO *)
+#<div># *)
+Import Ring_polynom.
 
-(** #</div></div># *)
+Lemma ring_correct
+  (R : comRingType)
+  (* `l` below is called a variable map, whose i-th item gives the            *)
+  (* interpretation of the variable i.                                        *)
+  (l : seq R)
+  (* `pe1` and `pe2` below are polynomial expressions with integer            *)
+  (* coefficients, representing the LHS and RHS of the equation to prove.     *)
+  (pe1 pe2 : PExpr Z) :
+  (* If `pe1` and `pe2` normalized to polynomials (in horner normal form) are *)
+  (* equal,                                                                   *)
+  Peq Z.eqb (norm_subst 0%R 1%R Z.add Z.mul Z.sub Z.opp Z.eqb
+               (triv_div 0%R 1%R Z.eqb) 0 [::] pe1)
+            (norm_subst 0%R 1%R Z.add Z.mul Z.sub Z.opp Z.eqb
+               (triv_div 0%R 1%R Z.eqb) 0 [::] pe2) ->
+  (* `pe1` and `pe2` interpreted as values of the ring `R` are equal.         *)
+  PEeval 0%R 1%R +%R *%R (fun x y => (x - y)%R) -%R%R
+    (fun n => ((int_of_Z n)%:~R)%R) id (fun x n => (x ^+ N.to_nat n)%R) l pe1 =
+  PEeval 0%R 1%R +%R *%R (fun x y => (x - y)%R) -%R%R
+    (fun n => ((int_of_Z n)%:~R)%R) id (fun x n => (x ^+ N.to_nat n)%R) l pe2.
+Proof. exact: (@Internals.Rcorrect R 0 l [::]). Qed.
+(** #</div>#
 
+Let us apply the above lemma to the example below.
+
+#<div># *)
+Goal forall a b : int, ((a + b) ^+ 2 = a ^+ 2 + b ^+ 2 + 2 * a * b :> int)%R.
+Proof.
+move=> a b.
+(* Variable map:                                                              *)
+pose l := [:: a; b].
+(* Polynomial expressions representing the LHS and RHS, where numbers `1` and *)
+(* `2` given as arguments of `PEX` represents 1st and 2nd variables, namely,  *)
+(* `a` and `b`:                                                               *)
+pose pe1 : PExpr Z := PEpow (PEadd (PEX _ 1) (PEX _ 2)) 2.
+pose pe2 : PExpr Z := PEadd (PEadd (PEpow (PEX _ 1) 2) (PEpow (PEX _ 2) 2))
+                        (PEmul (PEmul (PEc 2%coqZ) (PEX _ 1)) (PEX _ 2)).
+(* Apply the reflection lemma with the variable map and polynomial            *)
+(* expressions given above:                                                   *)
+apply: (@ring_correct _ [:: a; b] pe1 pe2).
+(* By reducing some subterms, we notice that we are comparing the same normal *)
+(* form:                                                                      *)
+rewrite /norm_subst [p1 in Peq _ p1 _]/= [p2 in Peq _ _ p2]/=.
+(* Actually, the entire goal reduces to `true`:                               *)
+rewrite /=.
+by [].
+Qed.
+(** #</div>#
+
+The only missing piece in turning this methodology into a fully-automated tactic
+is how to obtain the variable map and the polynomial expressions from the goal,
+which is called _reification_.
+
+#</div># *)
 (** -------------------------------------------- *)
 (** #<div class='slide'>#
 
 ** Further reading
 
-#</div>#
-*)
+- Benjamin Grégoire, Assia Mahboubi.
+  #<a href="https://hal.archives-ouvertes.fr/hal-00819484">Proving Equalities in a Commutative Ring Done Right in Coq</a>#.
+  In: TPHOLs 2005.
+
+  This paper explains the implementation of the [ring] tactic.
+- Frédéric Besson.
+  #<a href="http://people.rennes.inria.fr/Frederic.Besson/Fast_Reflexive_Arithmetics_Tactics.pdf">Fast Reflexive Arithmetic Tactics the Linear Case and Beyond</a>#.
+  In: TYPES 2006.
+
+  This paper explains the implementation of the [lia], [nia], [lra], [nra], and
+  [psatz] tactics.
+- Kazuhiko Sakaguchi.
+  #<a href="https://drops.dagstuhl.de/opus/volltexte/2022/16738/">Reflexive tactics for algebra, revisited</a>#.
+  In: ITP 2022.
+
+  This paper explains:
+  - a toy reflexive tactic for [zmodType] equations,
+  - its reification procedure implemented in Coq-Elpi, that works in the
+    presence of an inheritance hierarchy and overloaded operators
+    (cf. Lesson 5),
+  - how to preprocess homomorphisms by reflection efficiently, and
+  - the application of Mczify and Algebra Tactics to
+    #<a href="https://github.com/coq-community/apery">the formal proof of the irrationality of ζ(3)</a>#.
+
+- Enrico Tassi. #<a href="https://github.com/LPCIC/coq-elpi">Coq-Elpi</a>#.
+
+#</div># *)
