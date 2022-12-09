64cf687a01b5a7d985668474e32b02f25f44e430
Kazuhiko Sakaguchi <pi8027@gmail.com>
2022-12-09 11:10:17 +0100

    WIP Exercise 8

--- exercise8.v
+++ exercise8.v
@@ -13,40 +13,24 @@ Import GRing.Theory.
   ----
   ** Triangular number from Exercise session 2
 
-Use the [lia] tactic to simplify the following proofs.
+Use the [lia] and [nia] tactics to prove the following lemmas.
 *)
 Definition delta (n : nat) := (n.+1 * n)./2.
 
 Lemma deltaS (n : nat) : delta n.+1 = delta n + n.+1.
 Proof.
-(*
-rewrite /delta -addn2 mulnDl mulnC halfD.
-rewrite !oddM andbF add0n mul2n.
-suff -> : ((n.+1).*2)./2 = n.+1 by [].
-by rewrite -[RHS](half_bit_double n.+1 false).
-*)
 (*D*)rewrite /delta; lia.
 (*A*)Qed.
 
 Lemma leq_delta (m n : nat) : m <= n -> delta m <= delta n.
 Proof.
-(* by move=> H; apply: half_leq; apply: leq_mul. *)
 (*D*)move=> lemn; apply: half_leq; nia.
 (*A*)Qed.
 
+(** Use induction and the [lia] tactic to prove the following lemma. *)
+
 Lemma delta_square (n : nat) : (8 * delta n).+1 = n.*2.+1 ^ 2.
 Proof.
-(*
-elim: n => // n IHn.
-rewrite deltaS mulnDr -addSn IHn.
-rewrite doubleS -addn1 -addnS -addSn addn1.
-rewrite sqrnD -addnA /=.
-congr (_ + _).
-rewrite mulnS.
-rewrite [_ * 2]mulSn mulnDr addnA.
-congr (_ + _).
-by rewrite mulnCA -muln2 -!mulnA mulnC.
-*)
 (*D*)elim: n => // n IHn; rewrite deltaS mulnDr -addSn IHn; lia.
 (*A*)Qed.
 
@@ -54,49 +38,63 @@ by rewrite mulnCA -muln2 -!mulnA mulnC.
   ----
   ** Exercise 4 from Excersise session 3
 
-Use the [lia] or [ring] tactic to simplify the following proofs.
+Use induction and the [nia] tactic to prove the following lemmas.
 *)
 
-Lemma gauss_ex_nat1 (n : nat) : (\sum_(i < n) i).*2 = (n * n.-1)%N.
+Lemma gauss_ex_nat1 (n : nat) : (\sum_(i < n) i).*2 = n * n.-1.
 Proof.
-(*
-elim: n => [|n IH]; first by rewrite big_ord0.
-rewrite big_ord_recr /=.
-rewrite doubleD {}IH.
-rewrite -muln2 -mulnDr addn2 mulnC.
-by case: n.
-*)
 (*D*)elim: n => [|n IH]; first by rewrite big_ord0.
 (*D*)by rewrite big_ord_recr /= doubleD {}IH; nia.
 (*A*)Qed.
 
-Lemma gauss_ex_nat2 (n : nat) : (\sum_(i < n) i)%N = (n * n.-1)./2.
+Lemma gauss_ex_nat2 (n : nat) : \sum_(i < n) i = (n * n.-1)./2.
 Proof.
-(*
-elim: n => [|n IH]; first by rewrite big_ord0.
-rewrite big_ord_recr /= {}IH.
-case: n => // n.
-rewrite -add2n mulnDl halfD 2!oddM mul2n doubleK /=.
-by rewrite add0n addnC mulnC.
-*)
 (*D*)elim: n => [|n IH]; first by rewrite big_ord0.
 (*D*)rewrite big_ord_recr /= {}IH; nia.
 (*A*)Qed.
 
 (**
-  ----
-  ** Square pyramidal number
+Use induction and the [ring] tactic to prove the following lemma (it is also
+possible to use [lia] though).
+
+_Caveat_: there is a bug in the [ring] tactic of Algebra Tactics that it does
+not recognize [_.+1] (successor) inside [_%:R] (generic embedding of natural
+numbers to ring). You have to explicitly rewrite it by the [natr1] lemma for
+now.
 *)
-Lemma sum_square_p1 (n : nat) :
-  ((\sum_(i < n) i ^ 2) * 6 = n.-1 * n * (n * 2).-1)%N.
+
+Lemma gauss_ex_int1 (n : nat) (m : int) :
+  ((\sum_(i < n) (m + i%:R)) * 2 = (m * 2 + n%:R - 1) * n%:R)%R.
+Proof.
+(*D*)elim: n => [|n IH]; first by rewrite big_ord0 mulr0.
+(*D*)rewrite big_ord_recr /= mulrDl {}IH -natr1; ring.
+(*A*)Qed.
+
+(**
+  ----
+  ** Pyramidal numbers
+
+Use induction and the [nia] tactic to prove the following lemmas.
+*)
+Lemma sum_squares_p1 (n : nat) :
+  (\sum_(i < n) i ^ 2) * 6 = n.-1 * n * (n * 2).-1.
 Proof.
 (*D*)elim: n => [|n IHn]; first by rewrite big_ord0.
 (*D*)by rewrite mulSn add2n /= big_ord_recr /= mulnDl {}IHn; nia.
 (*A*)Qed.
 
-Lemma sum_square_p2 (n : nat) :
-  ((\sum_(i < n) i ^ 2) = (n.-1 * n * (n * 2).-1) %/ 6)%N.
+Lemma sum_squares_p2 (n : nat) :
+  \sum_(i < n) i ^ 2 = (n.-1 * n * (n * 2).-1) %/ 6.
 Proof.
 (*D*)elim: n => [|n IHn]; first by rewrite big_ord0.
 (*D*)rewrite mulSn add2n /= big_ord_recr /= {}IHn; nia.
+(*A*)Qed.
+
+Lemma sum_cubes (n : nat) : \sum_(i < n) i ^ 3 = ((n * n.-1) %/ 2) ^ 2.
+Proof.
+(*D*)elim: n => [|n IHn]; first by rewrite big_ord0.
+(*D*)rewrite big_ord_recr /= {}IHn; case: n => //= n.
+(*D*)rewrite [in RHS]mulnC -[in RHS]add2n mulnDr divnMDl // sqrnD.
+(*D*)rewrite -addnA addnCA mulnCA [2 * _]mulnC divnK; first lia.
+(*D*)rewrite dvdn2 oddM; lia.
 (*A*)Qed.
